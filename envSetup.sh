b2fa7f56bd615e6d09a35b96d9e5df3484a1fd20
Hanspeter Schaub <hanspeter.schaub@colorado.edu>
2021-03-02 11:32:56 -0700

    removed the un-needed `envSetup.sh` script.  Removed the depreciated opNav scenario files

--- envSetup.sh
+++
@@ -1,7 +0,0 @@
-pip install numpy
-# must have open-mpi installed ( on macOS, use  `brew install open-mpi` )
-pip install stable-baselines==2.7.0
-pip install keras==2.4.3
-pip install tensorflow==1.14.0
-pip install tqdm
-pip install colorama
