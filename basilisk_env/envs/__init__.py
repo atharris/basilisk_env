d70bb81e20381419a01d648f745185e9c133d8f2
Andrew Harris <anha5550@colorado.edu>
2021-03-04 10:15:49 -0700

    Removed reference to opnavEnv from __init__.py files.

--- basilisk_env/envs/__init__.py
+++ basilisk_env/envs/__init__.py
@@ -1,3 +1,2 @@
 from basilisk_env.envs.leoPowerAttitudeEnvironment import leoPowerAttEnv
-from basilisk_env.envs.opNavEnvironment import opNavEnv
 from basilisk_env.envs.earthObsEnvironment import EarthObsEnvironment
